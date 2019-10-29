56ebd16e3d776ab760e6d6bc0eeca046bce79cb8
Derek Hamilton <derek@dhamilton.codes>
2019-10-28 21:19:11 -0400

    Converted add and stash previews to python

--- git_gopher/__init__.py
+++ git_gopher/__init__.py
@@ -1,4 +1,5 @@
-from sys import argv
+from sys import argv, stdin
+from pygments.lexers.diff import DiffLexer
 from git_gopher.GitGopher import GitGopher
 from git_gopher.CommandFactory import CommandFactory
 from git_gopher.Options import Options
@@ -6,6 +7,7 @@ from git_gopher.VersionIncrementer import VersionIncrementer
 from git_gopher.Fzf import Fzf
 from git_gopher.GitDataGetter import GitDataGetter
 from git_gopher.AddPreview import AddPreview
+from git_gopher.Colorize import Colorize
 
 def main():
     git_gopher = GitGopher(CommandFactory(), Options())
@@ -17,3 +19,6 @@ def get_incremented_version():
 def add_preview():
     add_preview = AddPreview(GitDataGetter(Fzf()))
     print(add_preview.preview(argv[1]))
+
+def colorize():
+    print(Colorize().set_lexer(DiffLexer()).highlight(stdin.read()))
--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.4',  # Required
+    version='0.4.5',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -156,8 +156,8 @@ setup(
         'console_scripts': [
             'ggo=git_gopher:main',
             'ggo-get-incremented-version=git_gopher:get_incremented_version',
-            'ggo-add-preview=git_gopher:add_preview'
-            #'ggo-colorize=git_gopher:ggo-colorize'
+            'ggo-add-preview=git_gopher:add_preview',
+            'ggo-colorize=git_gopher:colorize'
         ],
     },
 
