8ebc2f317fc3a87c530888a2086f9800622baa20
Derek Hamilton <derek@dhamilton.codes>
2019-10-30 23:46:31 -0400

    get_branch_name test

--- test/test_GitDataGetter.py
+++ test/test_GitDataGetter.py
@@ -18,5 +18,16 @@ class TestGitDataGetter(unittest.TestCase):
         self.assertEqual(result, 'foo')
         command_runner.check_output.assert_called_once_with(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
 
+    def test_get_branch_name(self):
+        branch = 'foo'
+        command_runner = CommandRunner()
+        command_runner.check_output = MagicMock(return_value=b"branch | bar")
+        fzf = Fzf()
+        fzf.run = MagicMock(return_value="branch\t" + branch)
+
+        git_data_getter = git_data_getter = GitDataGetter(fzf, command_runner)
+        result = git_data_getter.get_branch_name()
+        self.assertEqual(result, branch)
+
 if __name__ == '__main__':
     unittest.main()
