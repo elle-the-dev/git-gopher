41eba7cd895a89bb58b6fd8006ebcaa6b7d757e3
Derek Hamilton <derek@dhamilton.codes>
2019-10-31 12:48:17 -0400

    get_tag_name_from_tags test

--- git_gopher/CommandRunner.py
+++ git_gopher/CommandRunner.py
@@ -1,8 +1,8 @@
 from subprocess import check_output, run
 
 class CommandRunner:
-    def check_output(self, cmd: list):
-        return check_output(cmd)
+    def check_output(self, cmd: list, input=None):
+        return check_output(cmd, input=input)
 
     def run(self, cmd: list):
         return run(cmd)
--- test/test_GitDataGetter.py
+++ test/test_GitDataGetter.py
@@ -4,6 +4,7 @@ from subprocess import check_output, CalledProcessError
 from git_gopher.Fzf import Fzf
 from git_gopher.CommandRunner import CommandRunner
 from git_gopher.GitDataGetter import GitDataGetter
+from git_gopher.NoTagsException import NoTagsException
 
 class TestGitDataGetter(unittest.TestCase):
 
@@ -28,6 +29,27 @@ class TestGitDataGetter(unittest.TestCase):
         git_data_getter = git_data_getter = GitDataGetter(fzf, command_runner)
         result = git_data_getter.get_branch_name()
         self.assertEqual(result, branch)
+
+    def test_get_tag_name_from_tags(self):
+        tag = 'foo'
+        command_runner = CommandRunner()
+        command_runner.check_output = MagicMock(return_value=b"tag | bar")
+        fzf = Fzf()
+        fzf.run = MagicMock(return_value="tag\t" + tag)
+
+        git_data_getter = git_data_getter = GitDataGetter(fzf, command_runner)
+        result = git_data_getter.get_tag_name()
+        self.assertEqual(result, tag)
+
+    def test_get_tag_name_from_tags_throws_exception_on_no_tags(self):
+        tag = 'foo'
+        command_runner = CommandRunner()
+        command_runner.check_output = MagicMock(return_value=b"")
+        fzf = Fzf()
+        fzf.run = MagicMock(return_value="tag\t" + tag)
+
+        git_data_getter = git_data_getter = GitDataGetter(fzf, command_runner)
+        self.assertRaises(NoTagsException, git_data_getter.get_tag_name)
 
 if __name__ == '__main__':
     unittest.main()
