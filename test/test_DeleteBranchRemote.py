2a9ceec1d93b4fd882d2788760829f2f60df14fc
derekhamilton <zurahn@gmail.com>
2019-11-08 21:28:46 -0500

    Fixed error on getting remote tags (#116)

--- test/test_DeleteBranchRemote.py
+++ test/test_DeleteBranchRemote.py
@@ -11,10 +11,10 @@ class TestDeleteBranchRemote(unittest.TestCase):
         remote = 'bar'
         branch = 'foo'
         branches = [branch]
-        git_data_getter = GitDataGetter(Fzf())
+        command_runner = CommandRunner()
+        git_data_getter = GitDataGetter(Fzf(), command_runner)
         git_data_getter.get_remote_name= MagicMock(return_value=remote)
         git_data_getter.get_branch_names_remote = MagicMock(return_value=branches)
-        command_runner = CommandRunner(git_data_getter)
         command_runner.run = MagicMock()
         delete_branch = DeleteBranchRemote(command_runner, git_data_getter)
         delete_branch.run()
