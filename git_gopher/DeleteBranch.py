16ab13f7b376df14eb7d6d1eb6032b32b2ee6623
derekhamilton <zurahn@gmail.com>
2019-11-11 23:31:58 -0500

    Added force delete prompt for not merged error (#122)

--- git_gopher/DeleteBranch.py
+++ git_gopher/DeleteBranch.py
@@ -10,4 +10,16 @@ class DeleteBranch(CommandInterface):
 
         if branches:
             for branch in branches:
-                self._hist_command_runer.run(['git', 'branch', '-d', branch])
+                response = self._hist_command_runer.run(['git', 'branch', '-d', branch]).decode()
+                if 'not fully merged' in response:
+                    self.confirm_force_delete(branch)
+
+    def confirm_force_delete(self, branch):
+        yesOrNo = None
+        while True:
+            yesOrNo = self._git_data_getter.get_confirm_force_delete(branch)
+            if yesOrNo == 'y':
+                self._hist_command_runer.run(['git', 'branch', '-D', branch])
+                break
+            elif yesOrNo == 'n':
+                break
--- test/test_DeleteBranch.py
+++ test/test_DeleteBranch.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, call
 from git_gopher.Fzf import Fzf
 from git_gopher.CommandRunner import CommandRunner
 from git_gopher.GitDataGetter import GitDataGetter
@@ -21,6 +21,22 @@ class TestDeleteBranch(unittest.TestCase):
         delete_branch.run()
 
         hist_command_runer.run.assert_called_once_with(['git', 'branch', '-d', branch])
+
+    def test_force_delete(self):
+        branch = 'foo'
+        branches = [branch]
+        command_runner = CommandRunner()
+        git_data_getter = GitDataGetter(Fzf(), command_runner)
+        git_data_getter.get_branch_names = MagicMock(return_value=branches)
+        git_data_getter.get_confirm_force_delete = MagicMock(return_value="y")
+
+        hist_command_runer = HistoryCommandRunner(git_data_getter, command_runner)
+        hist_command_runer.run = MagicMock(return_value=b"error: not fully merged")
+        delete_branch = DeleteBranch(hist_command_runer, git_data_getter)
+        delete_branch.run()
+
+        calls = [call(['git', 'branch', '-d', branch]), call(['git', 'branch', '-D', branch])]
+        hist_command_runer.run.assert_has_calls(calls)
 
 if __name__ == '__main__':
     unittest.main()
