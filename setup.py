4eb0427d31821fd46ad93ad279aea87b7f732b55
Elle <elle@elle.codes>
2023-08-24 12:57:04 -0400

    Added pygments to install requirements (#109)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.6',  # Required
+    version='0.4.7',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -124,7 +124,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['colorama'],  # Optional
+    install_requires=['colorama', 'pygments'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
