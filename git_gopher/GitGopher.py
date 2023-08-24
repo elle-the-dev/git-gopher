e2e045fcb65b1f9296cb33c1995497c4e366d8df
Elle <elle@elle.codes>
2023-08-24 12:57:04 -0400

    Fixed shortcuts for custom commands (#98)

--- git_gopher/GitGopher.py
+++ git_gopher/GitGopher.py
@@ -1,6 +1,7 @@
 from os import path
 from pathlib import Path
 from re import sub
+from subprocess import run
 
 class GitGopher():
     def __init__(self, command_factory, options):
@@ -15,9 +16,12 @@ class GitGopher():
         else:
             cmd = argv[1]
 
-        long_cmd = self._shortcut_to_long(cmd)
+        shortcuts = self._get_shortcuts()
+        if cmd in shortcuts.keys():
+            return run(shortcuts.get(cmd).split())
+
         try:
-            command = self._command_factory.make(long_cmd)
+            command = self._command_factory.make(cmd)
             response = command.run()
             if response:
                 print(response)
@@ -26,16 +30,13 @@ class GitGopher():
             print("Run the following to see a list of commands:\n")
             print("$ ggo")
 
-    def _shortcut_to_long(self, shortcut: str):
+    def _get_shortcuts(self):
         options = self._options.get().splitlines()
 
         switcher = {}
-
         for option in options:
             pieces = option.split('|')
-            switcher[pieces[1].strip()] = sub(r'^ggo ', '', pieces[0].strip())
+            switcher[pieces[1].strip()] = pieces[0].strip()
 
-        if shortcut in switcher.keys():
-            return switcher.get(shortcut)
+        return switcher
 
-        return shortcut
